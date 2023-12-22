2cb2dba38ded51f70c90be9e70be4781ab235678
vivekrajput-93 <viveksinghraajputt@gmail.com>
2023-12-22 12:15:58 +0530

    CRUD-API-PRODUCT

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,16 +1,44 @@
 import express from "express";
-import {requiredSignIn, isAdmin} from "../middlewares/authMiddleware.js";
-import formidable from "express-formidable"
-import { createProductController, getproductController } from "../controllers/productControllers.js";
+import { requiredSignIn, isAdmin } from "../middlewares/authMiddleware.js";
+import formidable from "express-formidable";
+import {
+  createProductController,
+  deleteProductController,
+  getSingleProductController,
+  getproductController,
+  productPhotoController,
+  updateProductController,
+} from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 // creating product routes
-
-router.post("/create-product", requiredSignIn, isAdmin, formidable(), createProductController)
+router.post(
+  "/create-product",
+  requiredSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
 
 // get product route
-router.get("/get-product", getproductController)
+router.get("/get-product", getproductController);
 
+// get single product route
+router.get("/get-product/:slug", getSingleProductController);
 
-export default  router;
+router.get("/product-photo/:pid", productPhotoController);
+
+// delete  product route
+router.delete("/delete-product/:pid", deleteProductController);
+
+// update product route
+router.put(
+  "/update-product/:pid",
+  requiredSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+export default router;
